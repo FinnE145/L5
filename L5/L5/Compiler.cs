5eb22b4866a2b0a43948ac1e114137262890ff10
FinnE145 <finn.emmerson@renertschool.ca>
2023-11-07 11:32:05 -0700

    Testing errors

--- L5/L5/Compiler.cs
+++ L5/L5/Compiler.cs
@@ -1,2 +1,10 @@
-﻿string s = "abcdef";
+﻿using L5;
+
+string s = @"abcdefg
+hijklmnop
+qrs
+tuv
+wxy&z".Replace("\r", "");
 Console.WriteLine(s);
+
+new Error("test.l5", "there was an error").Raise("newfile.l5", s, 8, 10);
